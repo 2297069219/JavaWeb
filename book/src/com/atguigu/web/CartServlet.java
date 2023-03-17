30ae487b8a9a7a7d6b138ddec8545f523bed0909
guan-programmer <2297069219@qq.com>
2023-03-17 14:44:40 +0800

    commit

--- book/src/com/atguigu/web/CartServlet.java
+++ book/src/com/atguigu/web/CartServlet.java
@@ -1,5 +1,12 @@
 package com.atguigu.web;
 
+import com.atguigu.pojo.Book;
+import com.atguigu.pojo.Cart;
+import com.atguigu.pojo.CartItem;
+import com.atguigu.service.BookService;
+import com.atguigu.service.impl.BookServiceImpl;
+import com.atguigu.utils.WebUtils;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,6 +22,9 @@ import java.io.IOException;
  * Version: v1.0
  */
 public class CartServlet extends BaseServlet{
+
+    private BookService bookService=new BookServiceImpl();
+
     /**
      * 加入购物车
      * @param req
@@ -23,6 +33,20 @@ public class CartServlet extends BaseServlet{
      * @throws IOException
      */
     protected void addItem(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("ok");
+        //获取请求参数 编号"
+        int id= WebUtils.parseInt(req.getParameter("id"), 0);
+        Book book = bookService.queryBookById(id);
+        CartItem cartItem=new CartItem(book.getId(),book.getName(),1,book.getPrice(),book.getPrice());
+        Cart cart = (Cart) req.getSession().getAttribute("cart");
+        if(cart==null){
+            cart=new Cart();
+            req.getSession().setAttribute("cart",cart);
+        }
+        cart.addItem(cartItem);
+
+        System.out.println(cart);
+        resp.sendRedirect(req.getContextPath());
+
+
     }
 }
